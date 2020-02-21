9935589dba4ee8a176508d1baa71b867edb5c3c1
onibrow <seiya.ono12@gmail.com>
2020-02-21 00:36:33 +0000

    Fix bootloader script and add calibration to pwm tester for quick voltage setting

--- bootloader.sh
+++ bootloader.sh
@@ -2,13 +2,14 @@ echo "Bootloader Tool v1.0"
 echo "Seiya Ono Fa'19"
 echo ""
 echo "Select Board to burn in"
-echo "[1] Pro Mni (3.3V)"
+echo "[1] Pro Mni (5V 16MHz)"
 echo "[2] Leonardo (5V)"
 read -p 'Board: ' board
 
 if [ $board -eq 1 ]
 then
-  /usr/lib/arduino-1.8.10/hardware/tools/avr/bin/avrdude -C/usr/lib/arduino-1.8.10/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -cstk500v1 -P/dev/ttyACM0 -b19200 -e -Ulock:w:0x3F:m -Uefuse:w:0xFD:m -Uhfuse:w:0xDE:m -Ulfuse:w:0xFF:m
+  /home/onibrow/.arduino15/packages/arduino/tools/avrdude/6.3.0-arduino17/bin/avrdude -C/home/onibrow/.arduino15/packages/arduino/tools/avrdude/6.3.0-arduino17/etc/avrdude.conf -v -patmega328p -cstk500v1 -P/dev/ttyACM0 -b19200 -e -Ulock:w:0x3F:m -Uefuse:w:0xFD:m -Uhfuse:w:0xDA:m -Ulfuse:w:0xFF:m
+  /home/onibrow/.arduino15/packages/arduino/tools/avrdude/6.3.0-arduino17/bin/avrdude -C/home/onibrow/.arduino15/packages/arduino/tools/avrdude/6.3.0-arduino17/etc/avrdude.conf -v -patmega328p -cstk500v1 -P/dev/ttyACM0 -b19200 -Uflash:w:/home/onibrow/.arduino15/packages/arduino/hardware/avr/1.8.2/bootloaders/atmega/ATmegaBOOT_168_atmega328.hex:i -Ulock:w:0x0F:m
 elif [ $board -eq 2 ]
 then
   /usr/lib/arduino-1.8.10/hardware/tools/avr/bin/avrdude -C/usr/lib/arduino-1.8.10/hardware/tools/avr/etc/avrdude.conf -v -patmega32u4 -cstk500v1 -P/dev/ttyACM0 -b19200 -e -Ulock:w:0x3F:m -Uefuse:w:0xcb:m -Uhfuse:w:0xd8:m -Ulfuse:w:0xff:m
--- oled_pmic/pwm_tester/pwm_tester.ino
+++ oled_pmic/pwm_tester/pwm_tester.ino
@@ -1,84 +1,86 @@
-/*
-  Fade
-
-  This example shows how to fade an LED on pin 9 using the analogWrite()
-  function.
-
-  The analogWrite() function uses PWM, so if you want to change the pin you're
-  using, be sure to use another PWM capable pin. On most Arduino, the PWM pins
-  are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Fade
-*/
-
 int led = 9;           // the PWM pin the LED is attached to
 int adc = A0;          // Reading in LED Voltage / 2
 int brightness = 255;    // how bright the LED is
 
-float led_v = 0.000;   // Voltage on the LED
+float led_v = 0.000;
 float target = 0.000;  // Target LED voltage
 int num_samples = 250; // Number of averages
 float bit_depth = 1023.000;
-float fudge = 0.05;
 
 int vcc = 5;
-String buff = String("deadbuffdead");
-long sum = 0;
-uint8_t fudge_judge;
+String buff = "";
+
+float pwm_voltages[256];
+unsigned long elapsed;
 
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 9 to be an output:
   Serial.begin(115200);
+  buff.reserve(200);
   pinMode(led, OUTPUT);
   pinMode(adc, INPUT);
+  analogWrite(led, 0);
+  calibrate_led();
+  elapsed = millis();
 }
 
-uint8_t fudge_zone() {
-  if (target > (led_v + fudge)) {
-    return 0;
-  } else if (target < (led_v - fudge)) {
-    return 1;
-  } else {
-    return 2;
+void calibrate_led() {
+  Serial.print("Calibrating");
+  for (int i = 0; i < 256; i++) {
+    analogWrite(led, i);
+    delay(0.5);
+    pwm_voltages[i] = read_voltage();
+    if (i % 10 == 0) {
+      Serial.print(".");
+    }
   }
+  Serial.println("\nCalibration Complete.");
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  sum = 0;
+float read_voltage() {
+  float sum = 0;
   for (int i = 0; i < num_samples; i++) {
     sum += analogRead(adc);
   }
-  led_v = vcc - (vcc * sum / num_samples / bit_depth * 2);
+  return (vcc - (vcc * sum / num_samples / bit_depth * 2));
+}
 
-  fudge_judge = fudge_zone();
-
-  if (fudge_judge == 2) {
-    ;
-  } else if (fudge_judge == 1 && brightness < 255) {
-    brightness += 1;
-  } else if (fudge_judge == 0 && brightness > 0) {
-    brightness -= 1;
+void write_voltage(float t) {
+  target = t;
+  if (target < pwm_voltages[255]) {
+    brightness = 255;
+  } else if (target > pwm_voltages[0]) {
+    brightness = 0;
+  } else {
+    for (int i = 0; i < 256; i++) {
+      if (pwm_voltages[i] < target) {
+        brightness = i;
+        break;
+      }
+    }
   }
-
   analogWrite(led, brightness);
+  delay(1);
+  led_v = read_voltage();
+}
 
-  if (Serial.available()) {
-    buff = Serial.readStringUntil('\n');
-//    Serial.println(buff);
-    target = buff.toFloat();
-    Serial.print("\nFudge: ");
-    Serial.println(fudge_judge);
-    Serial.print("Brightness: ");
+void serialEvent() {
+  buff = Serial.readStringUntil('\n');
+  //  Serial.print("SERIAL EVENT: ");
+  //  Serial.println(buff);
+  if (buff.compareTo("c") == 0) {
+    calibrate_led();
+  } else if (buff.toFloat() != 0) {
+    write_voltage(buff.toFloat());
+    Serial.print("\nDuty Cycle (8 bit): ");
     Serial.println(brightness);
-    Serial.print("Voltage: ");
+    Serial.print("Voltage (V): ");
     Serial.println(led_v);
-    Serial.print("Target: ");
+    Serial.print("Target  (V): ");
     Serial.println(target);
   }
-
-  delay(1);
+}
+void loop() {
+  delay(10);
 }
